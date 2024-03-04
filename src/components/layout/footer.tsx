dddc18f92c08b1bd12c92289adc4c44f3b158027
sba <asier.bayon@ti8m.ch>
2024-03-04 14:31:08 +0100

    change repo url

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,7 +4,11 @@ export default function Footer() {
   return (
     <footer className="mt-auto block pb-2 pt-4 text-sm text-zinc-500 dark:text-zinc-400">
       This website is{" "}
-      <a target="_blank" href={siteConfig.links.github} className="underline">
+      <a
+        target="_blank"
+        href={`${siteConfig.links.github}/asierbayon.com`}
+        className="underline"
+      >
         open source
       </a>
       . Do whatever you want with its code.
